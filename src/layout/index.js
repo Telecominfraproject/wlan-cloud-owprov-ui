1af16e03fcbf9c4bedcb479af2cdc9a077ef5bc0
Charles <charles.bourque96@gmail.com>
2021-08-31 18:19:02 -0400

    Changed endpoints keys

--- src/layout/index.js
+++ src/layout/index.js
@@ -36,7 +36,7 @@ const TheLayout = () => {
             <PageContainer t={t} routes={routes} redirectTo="/inventory" />
           </ToastProvider>
         </div>
-        <Footer t={t} version="0.8.23" />
+        <Footer t={t} version="0.8.24" />
       </div>
     </div>
   );
