09a973e203f1b9a8f741d3a12bc1bf62595f5ccf
Charles <charles.bourque96@gmail.com>
2022-03-29 21:24:28 +0100

    Label fix for ssid circle

--- src/pages/VenuePage/VenueChildrenCard/VenueLiveView/CirclePack/CircleComponent/SsidCircle.js
+++ src/pages/VenuePage/VenueChildrenCard/VenueLiveView/CirclePack/CircleComponent/SsidCircle.js
@@ -79,9 +79,9 @@ const SsidCircle = ({ node, style, handleClicks }) => {
                 <Tbody>
                   <Tr>
                     <Td>TX {t('analytics.bandwidth')}</Td>
-                    <Td>{bytesString(node.data.details.tx_bytes_bw.avg)}</Td>
-                    <Td>{bytesString(node.data.details.tx_bytes_bw.min)}</Td>
-                    <Td>{bytesString(node.data.details.tx_bytes_bw.max)}</Td>
+                    <Td>{bytesString(node.data.details.tx_bytes_bw.avg)}/s</Td>
+                    <Td>{bytesString(node.data.details.tx_bytes_bw.min)}/s</Td>
+                    <Td>{bytesString(node.data.details.tx_bytes_bw.max)}/s</Td>
                   </Tr>
                   <Tr>
                     <Td>TX {t('analytics.packets')} /s</Td>
