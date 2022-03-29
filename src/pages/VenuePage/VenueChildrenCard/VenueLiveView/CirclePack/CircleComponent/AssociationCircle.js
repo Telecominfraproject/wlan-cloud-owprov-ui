ae97c37af8931215aa0f8f392c97d23f28201ec3
Charles <charles.bourque96@gmail.com>
2022-03-29 16:07:13 +0100

    2.6.91: more data shown in association within circle graph, now using toLocaleString for displayed numbers

--- src/pages/VenuePage/VenueChildrenCard/VenueLiveView/CirclePack/CircleComponent/AssociationCircle.js
+++ src/pages/VenuePage/VenueChildrenCard/VenueLiveView/CirclePack/CircleComponent/AssociationCircle.js
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { WifiHigh } from 'phosphor-react';
 import { bytesString } from 'utils/stringHelper';
+import { useTranslation } from 'react-i18next';
 
 const propTypes = {
   node: PropTypes.instanceOf(Object).isRequired,
@@ -24,38 +25,62 @@ const propTypes = {
   style: PropTypes.instanceOf(Object).isRequired,
 };
 
-const AssociationCircle = ({ node, style, handleClicks }) => (
-  <Popover isLazy trigger="hover" placement="top">
-    <PopoverTrigger>
-      <animated.circle
-        key={node.id}
-        cx={style.x}
-        cy={style.y}
-        r={style.radius}
-        fill={node.data.details.color}
-        stroke="black"
-        strokeWidth="1px"
-        opacity={style.opacity}
-        onClick={handleClicks.onClick}
-      />
-    </PopoverTrigger>
-    <Portal>
-      <PopoverContent>
-        <PopoverArrow />
-        <PopoverCloseButton alignContent="center" mt={1} />
-        <PopoverHeader display="flex">
-          <WifiHigh weight="bold" size={24} />
-          <Text ml={2}>{node?.data?.name.split('/')[0]}</Text>
-        </PopoverHeader>
-        <PopoverBody>
-          <Heading size="sm">RSSI {node.data.details.rssi} db</Heading>
-          <Heading size="sm">TX {bytesString(node.data.details.tx_bytes)}</Heading>
-          <Heading size="sm">RX {bytesString(node.data.details.rx_bytes)}</Heading>
-        </PopoverBody>
-      </PopoverContent>
-    </Portal>
-  </Popover>
-);
+const AssociationCircle = ({ node, style, handleClicks }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Popover isLazy trigger="hover" placement="top">
+      <PopoverTrigger>
+        <animated.circle
+          key={node.id}
+          cx={style.x}
+          cy={style.y}
+          r={style.radius}
+          fill={node.data.details.color}
+          stroke="black"
+          strokeWidth="1px"
+          opacity={style.opacity}
+          onClick={handleClicks.onClick}
+        />
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverCloseButton alignContent="center" mt={1} />
+          <PopoverHeader display="flex">
+            <WifiHigh weight="bold" size={24} />
+            <Text ml={2}>{node?.data?.name.split('/')[0]}</Text>
+          </PopoverHeader>
+          <PopoverBody>
+            <Heading size="sm">RSSI {node.data.details.rssi} db</Heading>
+            <Heading size="md" as="u">
+              TX
+            </Heading>
+            <Heading size="sm">
+              {bytesString(node.data.details.tx_bytes)}, {bytesString(node.data.details.tx_bytes_bw)}/s
+            </Heading>
+            <Heading size="sm">
+              {node.data.details.tx_packets_bw.toLocaleString('en-US')} {t('analytics.packets')}/s
+            </Heading>
+            <Heading size="sm">MCS {node.data.details.tx_rate.mcs}</Heading>
+            <Heading size="sm">NSS {node.data.details.tx_rate.nss}</Heading>
+            <Heading size="md" as="u">
+              RX
+            </Heading>
+            <Heading size="sm">
+              {bytesString(node.data.details.rx_bytes)}, {bytesString(node.data.details.rx_bytes_bw)}/s
+            </Heading>
+            <Heading size="sm">
+              {node.data.details.rx_packets_bw.toLocaleString('en-US')} {t('analytics.packets')}/s
+            </Heading>
+            <Heading size="sm">MCS {node.data.details.rx_rate.mcs}</Heading>
+            <Heading size="sm">NSS {node.data.details.rx_rate.nss}</Heading>
+          </PopoverBody>
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  );
+};
 
 AssociationCircle.propTypes = propTypes;
 export default React.memo(AssociationCircle);
--- src/utils/stringHelper.js
+++ src/utils/stringHelper.js
@@ -1,14 +1,15 @@
 export const lowercaseFirstLetter = (initial) => (initial ? initial.charAt(0).toLowerCase() + initial.slice(1) : null);
 export const uppercaseFirstLetter = (initial) => (initial ? initial.charAt(0).toUpperCase() + initial.slice(1) : null);
 export const bytesString = (bytes, decimals = 2) => {
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   if (!bytes || bytes === 0) {
     return '0 B';
   }
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(k)), 10);
-  return `${parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`;
+  if (i < 0) return '1 B';
+  return `${parseFloat((bytes / k ** i).toFixed(dm).toLocaleString())} ${sizes[i]}`;
 };
 export const parseDbm = (value) => {
   if (value === undefined || value === null) return '-';
