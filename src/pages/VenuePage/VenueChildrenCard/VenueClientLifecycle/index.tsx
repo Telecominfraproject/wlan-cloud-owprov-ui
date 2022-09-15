75a1f8671f024f084dbf6ae0a213dffed33da4ba
Charles <charles.bourque96@gmail.com>
2022-09-15 10:06:16 +0100

    [WIFI-10849] Fix for client lifecycle finding all clients
    Signed-off-by: Charles <charles.bourque96@gmail.com>

--- src/pages/VenuePage/VenueChildrenCard/VenueClientLifecycle/index.tsx
+++ src/pages/VenuePage/VenueChildrenCard/VenueClientLifecycle/index.tsx
@@ -1,23 +1,36 @@
+import React, { useState } from 'react';
 import { Box } from '@chakra-ui/react';
 import LoadingOverlay from 'components/LoadingOverlay';
-import { useGetAnalyticsClients } from 'hooks/Network/Analytics';
+import { getAllClients } from 'hooks/Network/Analytics';
 import useDatePickers from 'hooks/useDatePickers';
-import React, { useState } from 'react';
 import { getHoursAgo } from 'utils/dateFormatting';
-import MacSearchBar from './MacSearchBar';
 import ClientLifecyleTable from './Table';
+import MacSearchBar from './MacSearchBar';
 
 interface Props {
   venueId: string;
 }
 
 const VenueClientLifecycle: React.FC<Props> = ({ venueId }) => {
+  const [macs, setMacs] = useState<string[] | undefined>();
   const [mac, setMac] = useState<string | undefined>();
-  const { data: macs, isFetching } = useGetAnalyticsClients({ venueId });
   const { start, end, timepickers, refreshId } = useDatePickers({ defaultStart: getHoursAgo(5 * 24) });
 
+  const getMacs = React.useCallback(async () => {
+    try {
+      const newMacs = await getAllClients(venueId);
+      return newMacs;
+    } catch (e) {
+      return undefined;
+    }
+  }, [venueId]);
+
+  React.useEffect(() => {
+    getMacs().then((res) => setMacs(res));
+  }, [getMacs]);
+
   return (
-    <LoadingOverlay isLoading={isFetching}>
+    <LoadingOverlay isLoading={!macs}>
       <Box minHeight="200px">
         <ClientLifecyleTable
           fromDate={Math.floor(start.getTime() / 1000)}
